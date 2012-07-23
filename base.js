8980b16ef16b01738a099b4cb2fd247db9f3b25c
Yuval Adam <yuv.adm@gmail.com>
2012-07-23 12:06:19 +0300

    initial raphael version

--- base.js
+++ base.js
@@ -1,13 +1,10 @@
-genMatrix = function(el, m, n) {
-    for (var i=0; i<m; i++) {
-        el.append('<div id="r' + i + '" class="mr"></div>');
-        var row = $('#r' + i);
-        for (var j=0; j<n; j++) {
-            var cell = row.append('<div id="c' + i + j + '" class="mc"></div>');
-        }
-    }
+gen = function() {
+    var paper = Raphael('canvas', 250, 250);
+    var circle = paper.circle(50, 40, 10);
+    circle.attr("fill", "#f00");
+    circle.attr("stroke", "#fff");
 };
 
 $(document).ready(function() {
-    genMatrix($('#viz'), 10, 10);
+    gen();
 });
